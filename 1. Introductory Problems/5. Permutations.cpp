85d6142ef23b4473b9a66becca2a68e2bc249fbb
Saidul Islam <60518662+fadilcse5bu@users.noreply.github.com>
2022-05-22 12:03:02 +0600

    Add files via upload

---
+++ 1. Introductory Problems/5. Permutations.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main()
+{
+  int n;
+  cin >> n;
+  if(n == 2 or n == 3) cout << "NO SOLUTION";
+  else {
+    for(int i = 2; i <= n; i += 2) {
+      cout << i << ' ';
+    }
+    for(int i = 1; i <= n; i += 2) {
+      cout << i << ' ';
+    }
+  }
+
+	return 0;
+}
